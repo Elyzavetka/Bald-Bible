104fb68ae527146e918993669308dd8dbc7e4752
Liza Tarasova <tarasova.liza.vic@gmail.com>
2024-03-29 23:03:22 +0000

    Deleted unnecessary comments from image.js

--- backend/controllers/image.js
+++ backend/controllers/image.js
@@ -1,29 +1,25 @@
-const Image = require("../models/image"); // import the Mongoose Image model Schmea from image.js
-const fs = require("fs"); // import the fs package
-// The fs package is used to modify the file system.
+const Image = require("../models/image"); 
+const fs = require("fs"); 
+
 exports.createImage = (req, res, next) => {
-  // call the post method, which adds a route to the router object, to handle POST requests to the /api/stuff endpoint
-  const url = req.protocol + "://" + req.get("host"); // set the url constant to the protocol and host of the request
-  req.body.image = JSON.parse(req.body.image); // parse the request body
+  const url = req.protocol + "://" + req.get("host"); 
+  req.body.image = JSON.parse(req.body.image); 
   const image = new Image({
-    // create a new Image object, using the Image model
-    // description: req.body.image.description, // set the description property of the Image object to the description property of the request body
-    ImageUrl: url + "/Images/" + req.file.filename, // set the ImageUrl property of the Image object to the ImageUrl property of the request body
-    userId: req.body.image.userId, // set the userId property of the Image object to the userId property of the request body
-  }); // end of const image = new Image({ ... })
+    ImageUrl: url + "/Images/" + req.file.filename, 
+    userId: req.body.image.userId, 
+  }); 
   image
-    .save() // call the save method, which saves the Image object to the database
-    .then(() => res.status(201).json({ message: "Image saved successfully!" })) // call the then method, which adds a callback function to the promise, to handle the success case
-    .catch((error) => res.status(400).json({ error: error })); // call the catch method, which adds a callback function to the promise, to handle the failure case
+    .save() 
+    .then(() => res.status(201).json({ message: "Image saved successfully!" })) 
+    .catch((error) => res.status(400).json({ error: error })); 
 };
 
 exports.getOneImage = (req, res, next) => {
-  // call the get method, which adds a route to the router object, to handle GET requests to the /:id endpoint
-  Image.findOne({ _id: req.params.id }) // call the findOne method, which returns a promise, which resolves to the Image object with the specified id
-    .then((image) => res.status(200).json(image)) // call the then method, which adds a callback function to the promise, to handle the success case
-    .catch((error) => res.status(404).json({ error: error })); // call the catch method, which adds a callback function to the promise, to handle the failure case
+  Image.findOne({ _id: req.params.id })
+    .then((image) => res.status(200).json(image)) 
+    .catch((error) => res.status(404).json({ error: error })); 
 };
-//handle function for to get all images from db
+
 exports.getAllImage = (req, res) => {
   Image.find({})
     .then((image) => res.status(200).json(image))
@@ -31,68 +27,55 @@ exports.getAllImage = (req, res) => {
 };
 
 exports.modifyImage = (req, res, next) => {
-  // call the put method, which adds a route to the router object, to handle PUT requests to the /:id endpoint
-  let image = new Image({ _id: req.params._id }); // create a new Image object, using the Image model
+  let image = new Image({ _id: req.params._id }); 
   if (req.file) {
-    // if the request contains a file
-    const url = req.protocol + "://" + req.get("host"); // set the url constant to the protocol and host of the request
-    req.body.image = JSON.parse(req.body.image); // parse the request body
+    const url = req.protocol + "://" + req.get("host"); 
+    req.body.image = JSON.parse(req.body.image); 
     image = {
-      // set the image object to an object with the following properties
-      _id: req.params.id, // set the _id property of the Image object to the id property of the request parameters
-      // description: req.body.image.description, // set the description property of the Image object to the description property of the request body
-      ImageUrl: url + "/Images/" + req.file.filename, // set the ImageUrl property of the Image object to the ImageUrl property of the request body
-      userId: req.body.image.userId, // set the userId property of the Image object to the userId property of the request body
-    }; // end of image = { ... }
+      _id: req.params.id, 
+      ImageUrl: url + "/Images/" + req.file.filename, 
+      userId: req.body.image.userId, 
+    }; 
   } else {
-    // if the request does not contain a file
     image = {
-      // set the image object to an object with the following properties
-      _id: req.params.id, // set the _id property of the Image object to the id property of the request parameters
-      // description: req.body.description, // set the description property of the Image object to the description property of the request body
-      ImageUrl: req.body.ImageUrl, // set the ImageUrl property of the Image object to the ImageUrl property of the request body
-      userId: req.body.userId, // set the userId property of the Image object to the userId property of the request body
-    }; // end of image = { ... }
-  } // end of if (req.file) { ... } else { ... }
-  Image.updateOne({ _id: req.params.id }, image) // call the updateOne method, which returns a promise, which resolves to the Image object with the specified id
+      _id: req.params.id, 
+      ImageUrl: req.body.ImageUrl, 
+      userId: req.body.userId, 
+    }; 
+  } 
+  Image.updateOne({ _id: req.params.id }, image) 
     .then(() =>
       res.status(201).json({ message: "Image updated successfully!" }),
-    ) // call the then method, which adds a callback function to the promise, to handle the success case
-    .catch((error) => res.status(400).json({ error: error })); // call the catch method, which adds a callback function to the promise, to handle the failure case
+    ) 
+    .catch((error) => res.status(400).json({ error: error })); 
 };
 
 exports.deleteImage = (req, res, next) => {
-  // call the delete method, which adds a route to the router object, to handle DELETE requests to the /:id endpoint
-  Image.findOne({ _id: req.params.id }) // call the findOne method, which returns a promise, which resolves to the Image object with the specified id
+  Image.findOne({ _id: req.params.id }) 
     .then((image) => {
-      // call the then method, which adds a callback function to the promise, to handle the success case
       if (!image) {
-        // if the image does not exist
-        return res.status(404).json({ error: "Image not found!" }); // return a 404 Not Found error
-      } // end of if (!image) { ... }
+        return res.status(404).json({ error: "Image not found!" }); 
+      } 
       if (image.userId !== req.auth.userId) {
-        // if the userId property of the Image object does not match the userId property of the request body
-        return res.status(401).json({ error: "Unauthorized request!" }); // return a 401 Unauthorized error
-      } // end of if (image.userId !== req.auth.userId) { ... }
-      const filename = image.ImageUrl.split("/Images/")[1]; // set the filename constant to the second element of the array returned by splitting the ImageUrl property of the Image object
+        return res.status(401).json({ error: "Unauthorized request!" }); 
+      } 
+      const filename = image.ImageUrl.split("/Images/")[1]; 
       fs.unlink("Images/" + filename, () => {
-        // call the unlink method, which deletes the file with the specified path
-        Image.deleteOne({ _id: req.params.id }) // call the deleteOne method, which returns a promise, which resolves to the Image object with the specified id
-          .then(() => res.status(200).json({ message: "Deleted!" })) // call the then method, which adds a callback function to the promise, to handle the success case
+        Image.deleteOne({ _id: req.params.id }) 
+          .then(() => res.status(200).json({ message: "Deleted!" })) 
           .catch((error) =>
             res.status(400).json({
               error: error,
             }),
-          ); // call the catch method, which adds a callback function to the promise, to handle the failure case
-      }); // end of .then(image => { ... })
+          ); 
+      }); 
     });
 };
 
 exports.getAllStuff = (req, res) => {
-  // call the get method, which adds a route to the router object, to handle GET requests to the /api/stuff endpoint
-  Image.find() // call the find method, which returns a promise, which resolves to an array of all the Image objects in the database
-    .then((images) => res.status(200).json(images)) // call the then method, which adds a callback function to the promise, to handle the success case
-    .catch((error) => res.status(400).json({ error: error })); // call the catch method, which adds a callback function to the promise, to handle the failure case
+  Image.find() 
+    .then((images) => res.status(200).json(images)) 
+    .catch((error) => res.status(400).json({ error: error })); 
 };
 
 exports.AddOrRemoveUserIDFromImageLikesArray = async (req, res) => {
@@ -100,27 +83,19 @@ exports.AddOrRemoveUserIDFromImageLikesArray = async (req, res) => {
   const userId = req.body.userId;
 
   try {
-    // Find the image by ID
     const image = await Image.findById(imageId);
 
     if (!image) {
       return res.status(404).json({ message: "Image not found" });
     }
-
-    // Check if the user has already liked the image
     const likedByUserIndex = image.likedByUser.indexOf(userId);
     console.log(imageId, userId);
     if (likedByUserIndex === -1) {
-      // If not found, add the user ID to the likedByUser array
       image.likedByUser.push(userId);
     } else {
-      // If found, remove the user ID from the likedByUser array
       image.likedByUser.splice(likedByUserIndex, 1);
     }
-    // Save the updated image
     const updatedImage = await image.save();
-
-    // Return the appropriate response
     const message =
       likedByUserIndex === -1
         ? "Like added successfully"
