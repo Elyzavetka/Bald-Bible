322dda3f35a4f44bf40f356c42412acceef92f88
Liza Tarasova <tarasova.liza.vic@gmail.com>
2024-03-29 23:40:21 +0000

    Deleted unnecessary comments from model user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose"); // import mongoose
-const uniqueValidator = require("mongoose-unique-validator"); // import mongoose-unique-validator
+const mongoose = require("mongoose"); 
+const uniqueValidator = require("mongoose-unique-validator"); 
 
 const userSchema = mongoose.Schema({
   firstName: { type: String, required: false },
@@ -9,12 +9,6 @@ const userSchema = mongoose.Schema({
   username: { type: String, required: false, unique: true },
 });
 
-userSchema.plugin(uniqueValidator); // call the plugin method, which adds the uniqueValidator plugin to the schema
-// The uniqueValidator plugin validates that the email property is unique before saving the user to the database.
-// The uniqueValidator plugin adds a pre-save validation hook for unique fields within a Mongoose schema.
-// The uniqueValidator plugin is not a validator in the strictest sense. It is a pre-save hook that validates unique fields.
-// The uniqueValidator plugin is not run by default, it needs to be installed and configured.
-// The uniqueValidator plugin is not run on update(), findOneAndUpdate() etc. by default, only on save().
-// The uniqueValidator plugin is not run on update() etc. by default, because it is a document middleware, not a query middleware.
+userSchema.plugin(uniqueValidator); 
 
-module.exports = mongoose.model("User", userSchema); // export the model, which is created from the schema, and give it the name 'User'
+module.exports = mongoose.model("User", userSchema); 
