d5a0171d778119c4aad5e7b582813d3570475cbd
Liza Tarasova <tarasova.liza.vic@gmail.com>
2024-10-10 21:28:56 +0100

    Delete unnecessary comments

--- backend/models/image.js
+++ backend/models/image.js
@@ -1,13 +1,12 @@
-const mongoose = require("mongoose"); 
+const mongoose = require("mongoose");
 
 const photoSchema = mongoose.Schema({
-  // description: { type: String, required: true }, // define a description property, which is a required string
-  imageUrl: { type: String }, 
-  userId: { type: String }, 
+  imageUrl: { type: String },
+  userId: { type: String },
   username: { type: String },
   dateAdded: { type: String },
   likedByUser: { type: [String], default: [] },
-  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
 });
 
-module.exports = mongoose.model("Images", photoSchema); 
+module.exports = mongoose.model("Images", photoSchema);
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const router = express.Router(); // call the Router method, which returns a router object, which we store in the router constant
+const router = express.Router();
 
-const userCtrl = require("../controllers/user"); // import the object exported from controllers/user.js
+const userCtrl = require("../controllers/user");
 
-router.post("/signup", userCtrl.signup); // call the post method, which adds a route to the router object, to handle POST requests to the /api/auth/signup endpoint referencing the signup function in controllers/user.js
-router.post("/login", userCtrl.login); // call the post method, which adds a route to the router object, to handle POST requests to the /api/auth/login endpoint referencing the login function in controllers/user.js
+router.post("/signup", userCtrl.signup);
+router.post("/login", userCtrl.login);
 
-module.exports = router; // export the router object, so it can be used by other code, e.g. app.js
+module.exports = router;
